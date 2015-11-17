bf7ce33f81e54c346eda66bc0098eef4ef84f0cf
jenniferkuhs <jen.kuhs@gmail.com>
2015-11-16 20:19:24 -0500

    added friendly ids for makes, categories, features

--- app/models/feature.rb
+++ app/models/feature.rb
@@ -1,4 +1,9 @@
 class Feature < ActiveRecord::Base
-	has_many :vehicle_features
+	extend FriendlyId
+  	friendly_id :name, use: [:slugged, :finders]
+
+  	has_many :vehicle_features
 	has_many :vehicles, through: :vehicle_features
+
+	validates_uniqueness_of :name
 end
